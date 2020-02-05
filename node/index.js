246ee35c6a0332c8d64ec60d4c5934885773fb9d
Bryce Ghim <buttonfly@gmail.com>
2020-02-05 17:05:38 +0900

    fix error when an app imports it

--- node/index.js
+++ node/index.js
@@ -1,5 +1,5 @@
 
 'use strict';
 
-// module.exports = require('bindings')('simplemedia');
-export default {Source, AudioDecoder, AudioRenderer} = require('bindings')('simplemedia');
+module.exports = require('bindings')('simplemedia');
+// export default {Source, AudioDecoder, AudioRenderer} = require('bindings')('simplemedia');
