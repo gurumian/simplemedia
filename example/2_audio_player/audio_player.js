73142533d3a27cf36461446cebf72178d1a8506d
Bryce Ghim <buttonfly@gmail.com>
2020-02-06 16:31:55 +0900

    added an example

---
+++ example/2_audio_player/audio_player.js
@@ -0,0 +1,119 @@
+'use strict'
+
+const {Source, AudioDecoder, AudioRenderer} = require('simplemedia');
+
+const adjust = 0;
+module.exports = class AudioPlayer {
+  constructor() {
+    this.source = new Source();
+    this.renderer = new AudioRenderer();
+    this.count=0;
+
+    this.source.trace = true;
+    this.renderer.trace = true;
+
+    this.isFirstFrame = true;
+    this.pts = 0;
+  }
+
+  async prepare() {
+    let source = this.source;
+    return new Promise((resolve, reject) => {
+      let fmt = source.prepare();
+      if(fmt) {
+        console.log(fmt);
+        if(source.hasAudio) {
+          let pid = source.audioPid;
+          console.log('pid: ' + pid);
+          let pidchannel = source.requestPidChannel({
+            pid : pid,
+          });
+      
+          this.decoder = new AudioDecoder();
+          this.decoder.prepare(fmt.streams[pid]['native']);
+          this.decoder.pidchannel = pidchannel;
+          this.decoder.trace = true;
+      
+          this.renderer.prepare({
+            samplerate: this.decoder.samplerate,
+            channels: this.decoder.channels,
+            channellayout: this.decoder.channellayout,
+            sampleformat: this.decoder.sampleformat,
+          });
+        }
+        resolve(fmt);
+      }
+      else {
+        reject();
+      }
+    });  
+  }
+
+  _decode() {
+    this.decoder.decode(frame => {
+      var delay = 0;
+      if(frame) {
+        if(this.isFirstFrame) {
+          this.pts = frame.pts;
+          this.isFirstFrame = false;
+        }
+        else {
+          delay = frame.pts - this.pts;
+          this.pts = frame.pts;
+        }
+
+        this.renderer.render(frame.data);
+        this.count++;
+       
+        delay = Math.floor(delay / 1000) - adjust;
+        setTimeout(()=>{
+          this._decode();
+        }, delay);
+      }
+      else {
+        console.log('null packet');
+        console.log('count: ' + this.count);
+        setTimeout(()=>{
+          if(this.onend)
+            this.onend();
+        });
+      }
+    });
+  }
+
+  /**
+   * @param {() => void} onend
+   */
+  set onended(onend) {
+    this.onend = onend;
+  }
+
+  start() {
+    this.source.start();
+    setTimeout(()=>{
+      this._decode();
+    }, 100);
+  }
+
+  stop() {
+    this.decoder.stop();
+    this.source.stop();
+  }
+
+  pause() {
+    this.source.pause();
+    this.decoder.pause();
+  }
+
+  resume() {
+    this.start();  
+  }
+
+
+  /**
+   * @param {string} datasource
+   */
+  set datasource(datasource) {
+    this.source.datasource = datasource;
+  }
+}
---
+++ example/2_audio_player/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const AudioPlayer = require('./audio_player');
+
+const test_media_uri='https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3';
+
+let player = new AudioPlayer();
+player.datasource = test_media_uri;
+player.prepare().then(resolve => {
+  console.log('prepared');
+  player.start();
+}).catch(err => {
+  console.log(err);
+});
+
+player.onend = (() => {
+  console.log('end-of-stream!');
+});
+
+console.log('Press any key to exit');
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.on('data', process.exit.bind(process, 0));
+
+//readAndDispatch();
+console.log('end!');
