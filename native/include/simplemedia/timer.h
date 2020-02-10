117969b5d5063843516fb3b96e7e00d99e0a2588
Bryce Ghim <buttonfly@gmail.com>
2020-02-10 14:31:57 +0900

    updated timer

--- native/include/simplemedia/timer.h
+++ native/include/simplemedia/timer.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef GURUM_TIMER_H_
+#define GURUM_TIMER_H_
 
 #include <cstdint>
 #include <chrono>
@@ -6,24 +7,15 @@
 namespace gurum {
 
 class Timer {
-private:
-	std::chrono::time_point<std::chrono::high_resolution_clock> target_time_;
-
-	int64_t proportional_{};
-	int64_t integral_{};
-	int64_t derivative_{};
-
-	constexpr static double P_{0.0};
-	constexpr static double I_{-1.0};
-	constexpr static double D_{0.0};
-
 public:
 	Timer();
 	void wait(int64_t period);
 	void update();
 
 private:
-	int64_t adjust() const;
+	std::chrono::time_point<std::chrono::high_resolution_clock> target_time_;
 };
 
 }
+
+#endif // GURUM_TIMER_H_
--- native/src/timer.cc
+++ native/src/timer.cc
@@ -13,25 +13,13 @@ void Timer::wait(const int64_t period) {
 
   const auto lag =
     std::chrono::duration_cast<std::chrono::microseconds>(
-      target_time_ - std::chrono::high_resolution_clock::now()) +
-      std::chrono::microseconds{adjust()};
+      target_time_ - std::chrono::high_resolution_clock::now());
 
   std::this_thread::sleep_for(lag);
-
-	const int64_t error =
-    std::chrono::duration_cast<std::chrono::microseconds>(
-      std::chrono::high_resolution_clock::now() - target_time_).count();
-  derivative_ = error - proportional_;
-  integral_ += error;
-  proportional_ = error;
 }
 
 void Timer::update() {
   target_time_ = std::chrono::high_resolution_clock::now();
-}
-
-int64_t Timer::adjust() const {
-  return P_ * proportional_ + I_ * integral_ + D_ * derivative_;
 }
 
 }
